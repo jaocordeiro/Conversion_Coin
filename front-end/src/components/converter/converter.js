7b2dca0b1b4b0acc0ff944344636e2cf758e9a85
João <joao.cordeiro@marknet.com.br>
2021-03-23 23:28:59 -0300

    Conectando o Front a API

--- front-end/src/components/converter/converter.js
+++ front-end/src/components/converter/converter.js
@@ -1,50 +1,89 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
 import './converter.css'
+import { urlBaseApi } from '../../config/api'
+import { Alert } from 'react'
 
+class Converter extends Component {
+    constructor(props){ 
+        super(props)
+        
+        this.state = {
+            sourceCoin: "USD",
+            destinyCoin: "",
+            optionsCoins: {},
+            convertedValue: ''
+        };
+        this.getCoinsFromApi();
+    }
 
-function Converter () {
-    return (
+    getCoinsFromApi = () => {
+        fetch(urlBaseApi + 'listCoins/').then(response =>
+          response.json()
+        ).then(json => {
+          this.setState({optionsCoins: json.currencies});
+        });
+      }
+
+      changeSourceCoin = (coin) => {
+        if (coin !== "USD") {
+          Alert.alert("Aviso!", " Dólar é a única moeda autorizada para há conversão");
+          return
+        }
+        this.setState({sourceCoin: coin});
+      }
+    
+      submitConversao = () => {
+        fetch(urlBaseApi + 'endpoint/', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            firstParam: 'yourValue',
+            secondParam: 'yourOtherValue'
+          })
+        });
+      }
+
+    render() {
+        return (
             
-            <div className= "converter">
-                <div className = "converterbar">
-                    <p><strong>Conversor de Moeda</strong></p>
-                </div>
+                <div className= "converter">
+                    <div className = "converterbar">
+                        <p><strong>Conversor de Moeda</strong></p>
+                    </div>
                     
-                    <Link className= "getout" as={Link} to="/">
-                        Sair
-                    </Link>
+                        <Link className= "getout" as={Link} to="/">
+                            Sair
+                        </Link>
                     
-                <div className="converter-title">
+                    <div className="converter-title">
 
-                    <div className = "converter-value">
-                        <h3>Dolar para Real</h3>
-                        <div className="converter-converterInputName">
-                            <input
-                                type="text"
-                                placeholder="EX: 5 = 27,69"
-                            />
+                        <div className = "converter-value">
+                            <h3>Dolar para Real</h3>
+                            <div className="converter-converterInputName">
+                                <input
+                                    type="text"
+                                    placeholder="EX: 5 = 27,69"
+                                    name= "converter"
+                                />
                                     
-                            <button className="buttonconverter" type="submit">
-                                Converter
-                            </button>
-                        </div>
-
-                        <h3>Dolar para Euro</h3>
-                        <div className="converter-converterInputName2">
-                            <input
-                                type="text"
-                                placeholder="EX: 5 = 12,58"
-                            />
-                            <button className="buttonconverter" type="submit">
-                                Converter
-                            </button>
-                        </div>
-                    </div>    
+                                <button className="buttonconverter" 
+                                    onClick = "teste()"  
+                                    type="submit"
+                                    value= "Coverter"
+                                >
+                                    Converter
+                                </button>
+                            </div>
+                        </div>    
+                    </div>
                 </div>
-            </div>
         )
     }
+}
 
 export default Converter
