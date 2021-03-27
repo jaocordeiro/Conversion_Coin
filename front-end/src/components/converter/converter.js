4b6e97d1b08cf76a7f056eb77d89e7410b7bf624
João <joao.cordeiro@marknet.com.br>
2021-03-26 21:05:31 -0300

    Validando os campos para seleção de moeda

--- front-end/src/components/converter/converter.js
+++ front-end/src/components/converter/converter.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { Link } from 'react-router-dom'
-import './converter.css'
-import { urlBaseApi } from '../../config/api'
-import axios from 'axios'
-//import { Alert, Select } from 'react'
+import { Link } from "react-router-dom";
+import "./converter.css";
+import { urlBaseApi } from "../../config/api";
+import axios from "axios";
+//import { Alert } from 'react'
 
 class Converter extends Component {
-    constructor(props){ 
-        super(props)
-      
-        this.state={
-          listcoins: [],
-          live: []
-        }
-    
-        this.getlistCoins = this.getlistCoins.bind(this);
-        //this.getlive = this.getlive.bind(this);
-      }
+  constructor(props) {
+    super(props);
 
-      componentDidMount(){
-        this.getlistCoins();
-        //this.getlive();
-      }
+    this.state = {
+      listcoins: [],
+      live: [],
+      saveFromCoin: "",
+      saveToCoins: "",
+    };
 
-      getlistCoins= async() => {
-        await axios.get(`${urlBaseApi}listCoins/`)
-        .then(res => {
-          this.setState({listcoins: res.data});
-          console.log(res.data);
-        }).catch((error) => {
-          console.log(error);
-        });
-      }
-      
+    this.getlistCoins = this.getlistCoins.bind(this);
+    //this.getlive = this.getlive.bind(this);
+  }
 
-      /* getlive= async() => {
+  componentDidMount() {
+    this.getlistCoins();
+    //this.getlive();
+  }
+
+  getlistCoins = async () => {
+    await axios
+      .get(`${urlBaseApi}listCoins/`)
+      .then((res) => {
+        this.setState({ listcoins: res.data["currencies"] });
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  /* getlive= async() => {
         await axios.get(`${urlBaseApi}live/`)
         .then(res => {
           this.setState({live: res.data});
@@ -43,51 +46,87 @@ class Converter extends Component {
         }).catch((error) => {
           console.log(error);
         });
-      }
- */
-    render() {
-        return (
-            
-                <div className= "converter">
-                    <div className = "converterbar">
-                        <p><strong>Conversor de Moeda</strong></p>
-                    </div>
-                    
-                        <Link className= "getout" as={Link} to="/">
-                            Sair
-                        </Link>
-                    
-                    <div className="converter-title">
+      }*/
 
-                        <div className = "converter-value">
-                            <h3>Converter</h3>
-
-                            <select className = "coins"
-                              //value = {this.state.listcoins}
-                              onChange = {this.changelistCoins}
-                            > 
-                            </select>
-
-                            <div className="converter-converterInputName">
-                                <input
-                                    type="text"
-                                    placeholder="Insira um valor"
-                                    name= "converter"
-                                />
-
-                                <button className="buttonconverter" 
-                                    //onClick = {this.getCoinsFromApi}
-                                    type="submit"
-                                    value= "Coverter"
-                                >
-                                    Converter
-                                </button>
-                            </div>
-                        </div>    
-                    </div>
-                </div>
-        )
+  selectFromCoins = () => {
+    var coins = document.getElementById("fromCoins").value;
+    if (coins !== "USD") {
+      alert("Dólar é a única moeda permitida para referência (USD)");
+      return;
     }
+
+    this.setState({ saveFromCoin: coins });
+  };
+
+  selectToCoins = () => {
+    var coins = document.getElementById("toCoins").value;
+
+    this.setState({ saveToCoin: coins });
+  };
+
+  validCoins = () => {
+    const { saveToCoin, saveFromCoin } = this.state;
+    if (saveToCoin === "" || saveFromCoin === "") {
+      alert("Escolhe as moedas");
+      return;
+    }
+  };
+
+  render() {
+    const { listcoins } = this.state;
+    console.log("Cheguei aquuuiiiiiiiiii", listcoins);
+
+    return (
+      <div className="converter">
+        <div className="converterbar">
+          <p>
+            <strong>Conversor de Moeda</strong>
+          </p>
+        </div>
+
+        <Link className="getout" as={Link} to="/">
+          Sair
+        </Link>
+
+        <div className="converter-title">
+          <div className="converter-value">
+            <h3>Conversor</h3>
+
+            <select id="fromCoins" onChange={this.selectFromCoins}>
+              <option value="">Selecione uma moeda:</option>
+              {Object.keys(listcoins).map((item) => {
+                return <option value={item}>{item}</option>;
+              })}
+            </select>
+
+            <select id="toCoins">
+              <option value="">Selecione uma moeda:</option>
+              {Object.keys(listcoins).map((item) => {
+                return <option value={item}>{item}</option>;
+              })}
+            </select>
+
+            <div className="converter-converterInputName">
+              <input
+                type="text"
+                placeholder="Insira um valor"
+                name="converter"
+              />
+
+              <button
+                className="buttonconverter"
+                onClick={this.validCoins}
+                type="submit"
+                value="Coverter"
+              >
+                Converter
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Converter
+export default Converter;
